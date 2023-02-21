00afe9ea27eb9615a3247590222d10b92bc913f4
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-02-21 14:08:12 +0100

    Added more styles to "MainContainer" component

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -2,7 +2,7 @@ import "./MainContainer.css";
 function MainContainer() {
   return (
     <main className="main-container">
-      <h1>I'm the MainContainer!</h1>
+      <h1>I'm the Main Container!</h1>
     </main>
   );
 }
